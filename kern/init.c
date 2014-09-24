0d143be9f9094e3a69005f1aa5eb08e07f28a541
Denis Efremov <efremov@ispras.ru>
2014-09-24 17:39:08 +0400

    Lab3 added.

--- kern/init.c
+++ kern/init.c
@@ -6,18 +6,10 @@
 
 #include <kern/monitor.h>
 #include <kern/console.h>
-
-// Test the stack backtrace function (lab 1 only)
-void
-test_backtrace(int x)
-{
-	cprintf("entering test_backtrace %d\n", x);
-	if (x > 0)
-		test_backtrace(x-1);
-	else
-		mon_backtrace(0, 0, 0);
-	cprintf("leaving test_backtrace %d\n", x);
-}
+#include <kern/env.h>
+#include <kern/trap.h>
+#include <kern/sched.h>
+#include <kern/cpu.h>
 
 void
 i386_init(void)
@@ -28,19 +20,26 @@ i386_init(void)
 	// Clear the uninitialized global data (BSS) section of our program.
 	// This ensures that all static/global variables start out zero.
 	memset(edata, 0, end - edata);
-
+	
 	// Initialize the console.
 	// Can't call cprintf until after we do this!
 	cons_init();
 
 	cprintf("6828 decimal is %o octal!\n", 6828);
+	cprintf("END: %p\n", end);
 
-	// Test the stack backtrace function (lab 1 only)
-	test_backtrace(5);
+	// user environment initialization functions
+	env_init();
 
-	// Drop into the kernel monitor.
-	while (1)
-		monitor(NULL);
+#ifdef CONFIG_KSPACE
+	// Touch all you want.
+	ENV_CREATE_KERNEL_TYPE(prog_test1);
+	ENV_CREATE_KERNEL_TYPE(prog_test2);
+	ENV_CREATE_KERNEL_TYPE(prog_test3);
+#endif
+
+	// Schedule and run the first user environment!
+	sched_yield();
 }
 
 
@@ -90,3 +89,4 @@ _warn(const char *file, int line, const char *fmt,...)
 	cprintf("\n");
 	va_end(ap);
 }
+
